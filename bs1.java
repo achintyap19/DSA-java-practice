663fe82fb78663172ecdf97bc0eba4644e776777
Achintya <achintya.pathak1306@gmail.com>
2026-05-15 13:59:39 +0530

    binary search for sqrt of an integer

---
+++ bs1.java
@@ -0,0 +1,18 @@
+class bs1 {
+    public static int squareroot(int n){
+        int low=0;
+        int high=n;
+        int ans=-1
+        while(low<high){
+            int mid = low+((high-low)/2);
+            if(mid*mid<=n){
+                ans=mid;
+                low = mid+1;
+            }
+            else{
+
+                high=mid-1;
+            }
+        }return ans;
+    }
+}
